41e82e902f530541b2be79ad3d86b7695d9d1b43
Fernando Val <aaromnido@gmail.com>
2018-05-30 00:21:49 +0200

    Fix responsive menu wrong behavior

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,22 +1,15 @@
 
 $(document).ready(function(){
-  // var closeButton = '<img src="/images/icon-close.svg" alt="Icon Close" id="close-button">';
-
-  // $( '#main-nav-mobile' ).click(function() {
-  //   event.preventDefault();
-  //   $('#main-nav-desktop').append( closeButton );
-  //   $('#main-nav-desktop').slideToggle( "slow", "linear" );
-
-  // });
 
   var responsiveMenuContainer = '<div class="responsive-menu-container hidden"></div>';
   var closeButton = '<div class="close-button"><span><img src="/images/icon-close.svg"></span></div>';
-  var menu = $('#main-nav-desktop');
+  var menu = $('.main-nav-desktop');
 
 
   $("body").prepend(responsiveMenuContainer);
   $(".responsive-menu-container").append(closeButton);
-  $(".responsive-menu-container").append(menu);
+  $( menu ).clone().appendTo( ".responsive-menu-container" );
+  //$(".responsive-menu-container").clone().append(menu);
 
 
   $( '#responsive-button' ).click(function() {
@@ -27,5 +20,6 @@ $(document).ready(function(){
     $('.responsive-menu-container').toggleClass('hidden');
   });
 
+  console.log("cargado");
 });
-console.log("cargado");
+
